7ceb1c0ec2d01a3b6bb83d2b7d75abb83a25152a
Marcel <wi24b041@technikum-wien.at>
2025-07-07 13:25:40 +0200

    add unit test for class Board and TicTacToe

--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -35,5 +35,39 @@ class BoardTest {
         board.place(0,0,'X');
         assertFalse(board.isFull());
     }
+    @Test
+    public void testPlaceInsertsMarkerCorrectly(){
+        Board board = new Board();
+        board.place(1,1,'X');
+        assertEquals('X',board.getCell(1,1),"Marker sollte korrekt gesetzt werden.");
+    }
+    @Test
+    public void testClearResetsBoard(){
+        Board board = new Board();
+        board.place(0,0,'O');
+        board.clear();
+        assertTrue(board.isCellEmpty(0,0),"Nach dem loeschen sollte die Zelle leer sein.");
+    }
+    @Test
+    public void testIsFullReturnsTrueIfBoardIsFull(){
+        Board board = new Board();
+        char mark = 'X';
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+            board.place(i,j,mark);
+               assertTrue(board.isFull(),"Das Board sollte voll sein.");
+
+    }
+    @Test
+    public void testGetCellReturnsCorrectMarker(){
+        Board board = new Board();
+        board.place(2,1,'O');
+        assertEquals('O',board.getCell(2,1),"Marker sollte gesezt sein.");
+    }
+    @Test
+    public void testGetCellReturnsEmptyIfUnoccupied(){
+        Board board = new Board();
+        assertEquals(' ',board.getCell(0,0),"Leere Zelle sollte ' ' zurueckgeben.");
+    }
 
 }
--- src/test/java/org/example/TicTacToeTest.java
+++ src/test/java/org/example/TicTacToeTest.java
@@ -28,5 +28,42 @@ class TicTacToeTest {
         assertEquals('X',game.getCurrentPlayer().getMarker());
         assertFalse(game.hasWinner(),"hasWinner sollte false zurueckgeben, wenn kein Spieler gewonne hat.");
     }
+    @Test
+    public void testSwitchCurrentPlayerChangesPlayer(){
+        TicTacToe game = new TicTacToe();
+        Player first = game.getCurrentPlayer();
+        game.switchCurrentPlayer();
+        assertNotEquals(first, game.getCurrentPlayer(),"Der aktuelle Spieler sollte gewchselt worden sein.");
+    }
+    @Test
+    public void tetSwitchCurrentPlayerTwiceReturnsOriginalPlayer(){
+        TicTacToe game = new TicTacToe();
+        Player first = game.getCurrentPlayer();
+        game.switchCurrentPlayer();
+        game.switchCurrentPlayer();
+        assertEquals(first, game.getCurrentPlayer(), "Nach zwei Wechsel sollte der urspruengliche Spieler wieder dran sein.");
+    }
+    @Test
+    public void testGetBoardNotNull(){
+        TicTacToe game = new TicTacToe();
+        assertNotNull(game.getBoard(), "Das Spielfeld sollte nicht null sein.");
+    }
+    @Test
+    public void testGetBoardConsistency(){
+        TicTacToe game = new TicTacToe();
+        Board board = game.getBoard();
+        board.place(1,1,'O');
+        assertEquals('O',game.getBoard().getCell(1,1),"Aenderung am Board sollte erhalen bleiben.");
+    }
+    @Test
+    public void testCurrentPlayerInitialIsPlayer1(){
+        TicTacToe game = new TicTacToe();
+        assertEquals('X',game.getCurrentPlayer().getMarker(),"Der erste Spieler sollte X sein");
+    }
+    @Test
+    public void testCurrentPlayerNotNull(){
+        TicTacToe game = new TicTacToe();
+        assertNotNull(game.getCurrentPlayer(),"Der aktuelle Spieler sollte nciht null sein.");
+    }
 
 }
